c36e986fbfe9b16665c82568eed2d60f0e83cc58
SeenivasanSeeni <seeni0424@gmail.com>
2018-05-29 12:20:40 +0530

    modify Version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="PDaS",
-    version="1.0.0",
+    version="1.0.1",
     author="seeni",
     author_email="seeni0424@gmail.com",
     url="https://github.com/Seenivasanseeni/PyDaS",
